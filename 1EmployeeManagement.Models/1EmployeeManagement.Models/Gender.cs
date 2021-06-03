99c02bb9f18e7caf92e1e01211d42caf3a5d5f1f
LAPTOP-SGA48FVR\aljon <aljonakarson@gmail.com>
2021-06-03 12:02:19 +0300

    Init

---
+++ 1EmployeeManagement.Models/1EmployeeManagement.Models/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1EmployeeManagement.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
