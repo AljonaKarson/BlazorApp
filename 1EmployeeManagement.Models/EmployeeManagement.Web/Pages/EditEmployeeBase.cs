71ccfcb42b96fa9f065e2805c4a6091891d472eb
LAPTOP-SGA48FVR\aljon <aljonakarson@gmail.com>
2021-06-05 22:31:41 +0300

    video29

---
+++ 1EmployeeManagement.Models/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -0,0 +1,24 @@
+﻿using _1EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EditEmployeeBase : CompomentBase
+    {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        [Parameter]
+        public Employee Employee { get; set; } = new Employee();
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
